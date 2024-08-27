898f92a6b1e57d0bb2f4031f974b225d23bcc5de
omengoemmanuel <omengomanuel@gmail.com>
2024-08-27 13:31:14 +0300

    views creation

--- devices/views.py
+++ devices/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from serializer import shopserializer
+from .models import shop
+from rest_framework import viewsets
+
 
 # Create your views here.
+class shopview(viewsets.ModelViewSet):
+    queryset = shop.objects.all()
+    serializer_class = shopserializer
